63ea6f8e0a4969710701fcc3840d1edb96f9b774
Ayan Das Karmakar <coderayan1411@gmail.com>
2022-05-22 20:59:03 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,59 @@
+if (!('TODOs' in localStorage)) {
+    localStorage.setItem('TODOs', JSON.stringify([]));
+}
+
+showTODOs();
+
+function addTODO() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let TODOs = JSON.parse(localStorage.getItem('TODOs'));
+
+    let newTODO = [title, description];
+    TODOs.push(newTODO);
+    localStorage.TODOs = JSON.stringify(TODOs);
+
+    showTODOs();
+}
+
+function showTODOs() {
+    let TODOs = JSON.parse(localStorage.getItem('TODOs'));
+    let TODOs_list_html = '';
+
+    TODOs.forEach((TODO, index) => {
+        let title = TODO[0];
+        let description = TODO[1];
+
+        let TODO_html = `
+        <tr>
+            <th scope="row">${index + 1}</th>
+            <td>${title}</td>
+            <td>${description}</td>
+            <td>
+                <button type="button" class="btn btn-danger btn-sm py-1 px-2" onclick="deleteTODO(${index})"><i
+                        class="bi bi-trash3"></i></button>
+            </td>
+        </tr>
+        `;
+
+        TODOs_list_html += TODO_html;
+    });
+
+    document.getElementById('showTODOs').innerHTML = TODOs_list_html;
+}
+
+function clearList() {
+    if (confirm("Do you want to clear the list?")) {
+        localStorage.clear();
+        localStorage.setItem('TODOs', JSON.stringify([]));
+        showTODOs();
+    }
+}
+
+function deleteTODO(TODOIndex) {
+    let TODOs = JSON.parse(localStorage.getItem('TODOs'));
+    TODOs.splice(TODOIndex, 1);
+
+    localStorage.TODOs = JSON.stringify(TODOs);
+    showTODOs();
+}
---
+++ readme.md
@@ -0,0 +1,30 @@
+# TODO List
+
+## Description
+
+It&#39;s a web **TODO List** project. Here you can set your TODOs to remember and remove them if done.
+
+
+## Created With
+
+This project is created using the following **markup languages**, **scripting language** and **framework**:
+
+- HTML
+- CSS
+- Javascript
+- [Bootstrap](https://www.getbootstrap.com/)
+
+## Created By
+
+This project is created by the following developers:
+
+- [Ayan Das Karmakar](https://github.com/Coder-Ayan)
+
+## Installation
+
+Install the **TODO List** with git:
+
+```bash
+  git clone git@github.com:Coder-Ayan/TODO-List.git
+  cd TODO-List
+```
